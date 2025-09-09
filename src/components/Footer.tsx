edb13fd26bbf91817917810e33bcdda7de77905f
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-09-09 12:16:01 +0200

    ✨ feat: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,13 @@
+import { FooterVariation } from "@digi/arbetsformedlingen";
+import { DigiFooter } from "@digi/arbetsformedlingen-react";
+
 export const Footer = () => {
-  return <>Footer!</>;
+  return (<>
+  <DigiFooter afVariation={FooterVariation.SMALL}>
+    <div slot="content-bottom-right">
+      <p>© 2025 Kodbanken - Branch Out</p>
+    </div>
+  </DigiFooter>
+  </>
+  );
 };
