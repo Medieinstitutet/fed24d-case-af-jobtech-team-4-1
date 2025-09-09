bac6636f72c2a3674e6db8d6946130f4ea40ca46
matildasoderhall <soderhall.matilda@gmail.com>
2025-09-09 14:24:02 +0200

    📝 docs: add temporary code to try fetch call

--- src/pages/Backend.tsx
+++ src/pages/Backend.tsx
@@ -1,3 +1,26 @@
 export const Backend = () => {
-  return <></>;
+
+  return <></>
 };
+
+//   const [jobs, setJobs] = useState<IAd[]>([]);
+
+//   useEffect(() => {
+//   getBackend().then((data) => {
+//     setJobs(data);
+//   });
+// }, []);
+  
+//   return (
+//     <div>
+//       <h2>Job Ads</h2>
+//       <ul>
+//         {jobs.map((job) => (
+//           <li key={job.id}>
+//             {job.headline} — {job.employer?.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+
