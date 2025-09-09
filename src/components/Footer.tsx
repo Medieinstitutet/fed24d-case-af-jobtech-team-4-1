635530a5e4ccc5f821f0399bff0a0a41081090da
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-09-09 12:29:18 +0200

    ✨ feat: add footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@ import { FooterVariation } from "@digi/arbetsformedlingen";
 import { DigiFooter } from "@digi/arbetsformedlingen-react";
 
 export const Footer = () => {
-  return (<>
+  return (
+  <>
   <DigiFooter afVariation={FooterVariation.SMALL}>
     <div slot="content-bottom-right">
       <p>© 2025 Kodbanken - Branch Out</p>
