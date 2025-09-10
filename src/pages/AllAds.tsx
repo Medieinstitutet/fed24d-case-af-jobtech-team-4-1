ebc42a80c91441428ccb211baece3cfaeff033d4
Yuliia-fed23 <147343499+Yuliia-fed23@users.noreply.github.com>
2025-09-10 10:09:39 +0200

    🥳 did filter & search

--- src/pages/AllAds.tsx
+++ src/pages/AllAds.tsx
@@ -1,3 +1,15 @@
+import { SearchFilters } from '../components/SearchFilters';
+import type { SearchState } from '../models/ISearchState';
+
 export const AllAds = () => {
-  return <></>;
+  const handleFiltersChange = (filters: SearchState) => {
+    console.log('Filters:', filters);
+  };
+
+  return (
+    <div>
+      <SearchFilters onFiltersChange={handleFiltersChange} />
+      <h2>Hej</h2>
+    </div>
+  );
 };
