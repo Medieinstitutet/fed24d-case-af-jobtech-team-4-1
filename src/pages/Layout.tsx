9e713af8783dc75fccba4cf37f9d0c1aec250781
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-09-10 09:12:47 +0200

    ✨ feat: add navigation
    and some styling

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router";
 import { Footer } from "../components/Footer";
 import { DigiLayoutBlock } from "@digi/arbetsformedlingen-react";
 import { LayoutBlockVariation } from "@digi/arbetsformedlingen";
+import { Navigation } from "../components/Navigation";
 
 export const Layout = () => {
   return (
     <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
-      <header />
+      <header>
+        <Navigation />
+      </header>
       <main>
         <Outlet />
       </main>
