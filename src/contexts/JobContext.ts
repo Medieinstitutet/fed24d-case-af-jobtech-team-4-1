39e7ef651cba554d21f99ccb82babfcce8984cbb
matildasoderhall <soderhall.matilda@gmail.com>
2025-09-11 15:36:13 +0200

    ✨ feat: export types and add context provider

--- src/contexts/JobContext.ts
+++ src/contexts/JobContext.ts
@@ -3,14 +3,14 @@ import type { IAd } from "../models/IAd";
 import { OccupationId } from "../services/jobAdService";
 import type { JobAction } from "../reducers/JobReducer";
 
-type JobState = {
+export type JobState = {
   [OccupationId.BACKEND]: IAd[];
   [OccupationId.FRONTEND]: IAd[];
   [OccupationId.FULLSTACK]: IAd[];
   [OccupationId.ALL]: IAd[];
 };
 
-const initialState: JobState = {
+export const initialState: JobState = {
   [OccupationId.BACKEND]: [],
   [OccupationId.FRONTEND]: [],
   [OccupationId.FULLSTACK]: [],
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,19 +3,26 @@ import { Footer } from "../components/Footer";
 import { DigiLayoutBlock, DigiTypography } from "@digi/arbetsformedlingen-react";
 import { LayoutBlockVariation } from "@digi/arbetsformedlingen";
 import { Navigation } from "../components/Navigation";
+import { JobContext } from "../contexts/JobContext";
+import { useReducer } from "react";
+import { JobReducer } from "../reducers/JobReducer";
+import { initialState } from "../contexts/JobContext";
 
 export const Layout = () => {
+  const [jobs, dispatch] = useReducer(JobReducer, initialState);
   return (
-    <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
-      <DigiTypography>
-        <header>
-          <Navigation />
-        </header>
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </DigiTypography>
-    </DigiLayoutBlock>
+    <JobContext.Provider value={{ jobs, dispatch }}>
+      <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+        <DigiTypography>
+          <header>
+            <Navigation />
+          </header>
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </DigiTypography>
+      </DigiLayoutBlock>
+    </JobContext.Provider>
   );
 };
