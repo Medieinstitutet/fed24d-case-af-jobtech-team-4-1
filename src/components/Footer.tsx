86f6d2c6e4a19385dbb8cf98c85432339cab93e4
matildasoderhall <soderhall.matilda@gmail.com>
2025-09-16 15:12:46 +0200

    🎨 format: format prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,11 @@ import { DigiFooter } from "@digi/arbetsformedlingen-react";
 import "./Footer.scss";
 
 export const Footer = () => {
-
   return (
     <DigiFooter afVariation={FooterVariation.SMALL} className="site-footer">
       <div className="site-footer__content">
         <p>© 2025 Kodbanken - Branch Out</p>
       </div>
     </DigiFooter>
-  ) 
+  );
 };
