b2ccf41a2f796ae79b4dbab8473f2ea059b59339
matildasoderhall <soderhall.matilda@gmail.com>
2025-09-16 14:51:48 +0200

    🐛 fix: change brackets and add return

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import { FooterVariation } from "@digi/arbetsformedlingen";
 import { DigiFooter } from "@digi/arbetsformedlingen-react";
 import "./Footer.scss";
 
-export const Footer = () => (
-  <DigiFooter afVariation={FooterVariation.SMALL} className="site-footer">
-    <div className="site-footer__content">
-      <p>© 2025 Kodbanken - Branch Out</p>
-    </div>
-  </DigiFooter>
-);
+export const Footer = () => {
+
+  return (
+    <DigiFooter afVariation={FooterVariation.SMALL} className="site-footer">
+      <div className="site-footer__content">
+        <p>© 2025 Kodbanken - Branch Out</p>
+      </div>
+    </DigiFooter>
+  ) 
+};
